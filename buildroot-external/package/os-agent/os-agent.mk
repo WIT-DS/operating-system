de32b99da9df1b4647bc2df27e38d1dcb397ad89
Damien S <damien.senechal@wit.fr>
2024-04-23 19:55:30 +0200

    change home-assistant to wit-ds

--- buildroot-external/package/os-agent/os-agent.mk
+++ buildroot-external/package/os-agent/os-agent.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 OS_AGENT_VERSION = 1.6.0
-OS_AGENT_SITE = $(call github,home-assistant,os-agent,$(OS_AGENT_VERSION))
+OS_AGENT_SITE = $(call github,wit-ds,os-agent,$(OS_AGENT_VERSION))
 OS_AGENT_LICENSE = Apache License 2.0
 OS_AGENT_LICENSE_FILES = LICENSE
 OS_AGENT_GOMOD = github.com/wit-ds/os-agent
--- buildroot-external/package/tempio/tempio.mk
+++ buildroot-external/package/tempio/tempio.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 TEMPIO_VERSION = 2021.09.0
-TEMPIO_SITE = $(call github,home-assistant,tempio,$(TEMPIO_VERSION))
+TEMPIO_SITE = $(call github,wit-ds,tempio,$(TEMPIO_VERSION))
 TEMPIO_LICENSE = Apache License 2.0
 TEMPIO_LICENSE_FILES = LICENSE
 TEMPIO_GOMOD = github.com/wit-ds/tempio
